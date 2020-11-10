4a30a4128815f4fcf31433784e23776574933378
Cristian Garcia <cg7497@gmail.com>
2020-11-10 07:56:15 -0600

    up to marker done

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,29 @@
-var map;
+let map;
+let coords = { lat: 39.9612, lng: -82.9988 };
 function initMap() {
+
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.9612, lng: -82.9988},
-        zoom: 10
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
     });
+
+    let marker = new google.maps.Marker({ position: coords, map: map, animation: google.maps.Animation.BOUNCE });
+
+    let image = {
+        url: '/custom_marker.png',
+        scaledSize: new google.maps.Size(50, 50)
+    };
+
+    let contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
+
+    let infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+
 }
